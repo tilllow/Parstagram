b5e2e0937e1bbf1365344c32299809ef90995d03
Ajuisiwon Azantilow <azantilow@fb.com>
2022-06-17 16:53:38 -0700

    Added the timestamp for every post.

--- app/src/main/java/com/example/loginactivity/PostsAdapter.java
+++ app/src/main/java/com/example/loginactivity/PostsAdapter.java
@@ -1,6 +1,7 @@
 package com.example.loginactivity;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.parse.ParseFile;
 
+import java.util.Date;
 import java.util.List;
 
 public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder> {
@@ -50,6 +52,7 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder> 
         private ImageView ivImage;
         private TextView createdAt;
         private ImageView ivProfileImage;
+        private TextView tvPostTimeStamp;
 
         public ViewHolder(@NonNull View itemView){
             super(itemView);
@@ -57,6 +60,7 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder> 
             ivImage = itemView.findViewById(R.id.ivImage);
             tvDescription = itemView.findViewById(R.id.tvFragmentDescription);
             ivProfileImage = itemView.findViewById(R.id.ivProfilePicture);
+            tvPostTimeStamp = itemView.findViewById(R.id.tvPostTimeStamp);
         }
 
         public void bind(Post post) {
@@ -64,11 +68,48 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder> 
             tvDescription.setText(post.getDescription());
             tvUsername.setText(post.getUser().getUsername());
             ivProfileImage.setImageResource(R.drawable.mickeymouse);
+            tvPostTimeStamp.setText(calculateTimeAgo(post.getCreatedAt()));
             ParseFile image = post.getImage();
             if (image != null){
                 Glide.with(context).load(post.getImage().getUrl()).into(ivImage);
             }
 
+        }
+
+        public String calculateTimeAgo(Date createdAt) {
+
+            int SECOND_MILLIS = 1000;
+            int MINUTE_MILLIS = 60 * SECOND_MILLIS;
+            int HOUR_MILLIS = 60 * MINUTE_MILLIS;
+            int DAY_MILLIS = 24 * HOUR_MILLIS;
+
+            try {
+                createdAt.getTime();
+                long time = createdAt.getTime();
+                long now = System.currentTimeMillis();
+
+                final long diff = now - time;
+                if (diff < MINUTE_MILLIS) {
+                    return "just now";
+                } else if (diff < 2 * MINUTE_MILLIS) {
+                    return "a minute ago";
+                } else if (diff < 50 * MINUTE_MILLIS) {
+                    return diff / MINUTE_MILLIS + " minutes ago";
+                } else if (diff < 90 * MINUTE_MILLIS) {
+                    return "an hour ago";
+                } else if (diff < 24 * HOUR_MILLIS) {
+                    return diff / HOUR_MILLIS + " hours ago";
+                } else if (diff < 48 * HOUR_MILLIS) {
+                    return "yesterday";
+                } else {
+                    return diff / DAY_MILLIS + " days ago";
+                }
+            } catch (Exception e) {
+                Log.i("Error:", "getRelativeTimeAgo failed", e);
+                e.printStackTrace();
+            }
+
+            return "";
         }
     }
 }
